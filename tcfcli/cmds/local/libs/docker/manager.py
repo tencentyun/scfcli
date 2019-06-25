180363f0788493ca74c67b9accbaebe137f5f11e
wikichen <wikichen@tencent.com>
2019-06-25 15:57:01 +0800

    add: --quiet option

--- tcfcli/cmds/local/libs/docker/manager.py
+++ tcfcli/cmds/local/libs/docker/manager.py
@@ -5,11 +5,14 @@ import click
 class ContainerManager(object):
     def __init__(self,
                  docker_network_id=None,
-                 skip_pull_image=False):
+                 skip_pull_image=False,
+                 is_quiet=False):
 
         self._docker_network_id = docker_network_id
         self._skip_pull_image = skip_pull_image
         self._docker_client = docker.from_env()
+        
+        self._is_quiet=is_quiet
 
     def run(self, container):
         image = container.image
@@ -17,7 +20,8 @@ class ContainerManager(object):
         if not self.has_image(image) or not self._skip_pull_image:
             self.pull_image(image)
         else:
-            click.secho('skip pull image %s' % image)
+            if not self._is_quiet:
+                click.secho('skip pull image %s' % image)
 
         if not container.is_exist():
             container.create()
@@ -29,10 +33,13 @@ class ContainerManager(object):
 
     def pull_image(self, image):
         try:
-            click.secho('pull image %s...' % image, nl=False)
+            if not self._is_quiet:
+                click.secho('pull image %s...' % image, nl=False)
             for _ in self._docker_client.api.pull(image, stream=True, decode=True):
-                click.secho('.', nl=False)
-            click.secho('\n')
+                if not self._is_quiet:
+                    click.secho('.', nl=False)
+            if not self._is_quiet:
+                click.secho('\n')
         except docker.errors.APIError as e:
             raise Exception('pull the docker image %s failed, %s' % (image, str(e)))
 
