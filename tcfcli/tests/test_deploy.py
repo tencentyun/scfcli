bf89e9ad3288524bc9101891a90f0fd4f587a0c1
jorgechen <jorgechen@tencent.com>
2019-07-11 22:24:41 +0800

    add deployt unit tests

--- tcfcli/tests/test_deploy.py
+++ tcfcli/tests/test_deploy.py
@@ -25,22 +25,117 @@ class TestDeploy(unittest.TestCase):
     def tearDown(self):
         super(TestDeploy, self).tearDown()
 
-    def test_deploy_default(self):
-        if os.path.exists(PythonDemoProjectPath):
-            shutil.rmtree(PythonDemoProjectPath)
+    def test_deploy_python27(self):
+        name = "hello_world_python27"
+        path = "./" + name
+        if os.path.exists(path):
+            shutil.rmtree(path)
 
         runner = CliRunner()
-        result = runner.invoke(init_cli.init, ['-N', '-l', PythonDemoGithub])
-        self.assertEqual(
-            '[+] Initializing project...\nTemplate: %s\nOutput-Dir: .\n[*] Project initialization is complete\n' % PythonDemoGithub,
-            result.output)
+        result = runner.invoke(init_cli.init, ['-N', '-r', 'python2.7', '-n', name, '-ns', 'default'])
+        self.assertIn('[*] Project initialization is complete\n', result.output)
         self.assertEqual(0, result.exit_code)
 
-        os.chdir(PythonDemoProjectPath)
-
-        result = runner.invoke(deploy_cli.deploy, ["-f"])
-        self.assertIn('Deploy function \'hello_world\' success', result.output)
+        result = runner.invoke(deploy_cli.deploy, ["-t", "./%s/template.yaml" % name, "-f"])
+        self.assertIn('Deploy function \'%s\' success' % name, result.output)
         self.assertEqual(0, result.exit_code)
+        if os.path.exists(path):
+            shutil.rmtree(path)
+
+    def test_deploy_python36(self):
+        name = "hello_world_python36"
+        path = "./" + name
+        if os.path.exists(path):
+            shutil.rmtree(path)
+
+        runner = CliRunner()
+        result = runner.invoke(init_cli.init, ['-N', '-r', 'python3.6', '-n', name, '-ns', 'default'])
+        self.assertIn('[*] Project initialization is complete\n', result.output)
+        self.assertEqual(0, result.exit_code)
+
+        result = runner.invoke(deploy_cli.deploy, ["-t", "./%s/template.yaml" % name, "-f"])
+        self.assertIn('Deploy function \'%s\' success' % name, result.output)
+        self.assertEqual(0, result.exit_code)
+        if os.path.exists(path):
+            shutil.rmtree(path)
+
+    def test_deploy_nodejs610(self):
+        name = "hello_world_nodejs610"
+        path = "./" + name
+        if os.path.exists(path):
+            shutil.rmtree(path)
+
+        runner = CliRunner()
+        result = runner.invoke(init_cli.init, ['-N', '-r', 'nodejs6.10', '-n', name, '-ns', 'default'])
+        self.assertIn('[*] Project initialization is complete\n', result.output)
+        self.assertEqual(0, result.exit_code)
+
+        result = runner.invoke(deploy_cli.deploy, ["-t", "./%s/template.yaml" % name, "-f"])
+        self.assertIn('Deploy function \'%s\' success' % name, result.output)
+        self.assertEqual(0, result.exit_code)
+        if os.path.exists(path):
+            shutil.rmtree(path)
+
+    def test_deploy_nodejs89(self):
+        name = "hello_world_nodejs89"
+        path = "./" + name
+        if os.path.exists(path):
+            shutil.rmtree(path)
+
+        runner = CliRunner()
+        result = runner.invoke(init_cli.init, ['-N', '-r', 'nodejs8.9', '-n', name, '-ns', 'default'])
+        self.assertIn('[*] Project initialization is complete\n', result.output)
+        self.assertEqual(0, result.exit_code)
+
+        result = runner.invoke(deploy_cli.deploy, ["-t", "./%s/template.yaml" % name, "-f"])
+        self.assertIn('Deploy function \'%s\' success' % name, result.output)
+        self.assertEqual(0, result.exit_code)
+        if os.path.exists(path):
+            shutil.rmtree(path)
+
+    def test_deploy_python27_namespace(self):
+        name = "hello_world_python27_ns"
+        path = "./" + name
+        if os.path.exists(path):
+            shutil.rmtree(path)
+
+        runner = CliRunner()
+        result = runner.invoke(init_cli.init, ['-N', '-r', 'python2.7', '-n', name, '-ns', 'default'])
+        self.assertIn('[*] Project initialization is complete\n', result.output)
+        self.assertEqual(0, result.exit_code)
+
+        result = runner.invoke(deploy_cli.deploy, ["-t", "./%s/template.yaml" % name, "-f"])
+        self.assertIn('Deploy function \'%s\' success' % name, result.output)
+        self.assertEqual(0, result.exit_code)
+
+        result = runner.invoke(deploy_cli.deploy, ["-t", "./%s/template.yaml" % name, "-f", "-ns", "default111"])
+        self.assertIn('Deploy function \'%s\' success' % name, result.output)
+        self.assertEqual(0, result.exit_code)
+
+        if os.path.exists(path):
+            shutil.rmtree(path)
+
+    def test_deploy_python27_cos(self):
+        name = "hello_world_python27_cos"
+        path = "./" + name
+        cos_bucket = "scf-function-test-hk"
+        new_namespace = "default111"
+        if os.path.exists(path):
+            shutil.rmtree(path)
+
+        runner = CliRunner()
+        result = runner.invoke(init_cli.init, ['-N', '-r', 'python2.7', '-n', name, '-ns', 'default'])
+        self.assertIn('[*] Project initialization is complete\n', result.output)
+        self.assertEqual(0, result.exit_code)
+
+        result = runner.invoke(deploy_cli.deploy, ["-t", "./%s/template.yaml" % name, "-f", "-ns", new_namespace, "-c",
+                                                   cos_bucket])
+        self.assertIn('Deploy function \'%s\' success' % name, result.output)
+        self.assertIn('to COS bucket \'%s\' success' % cos_bucket, result.output)
+        self.assertEqual(0, result.exit_code)
+
+        if os.path.exists(path):
+            shutil.rmtree(path)
 
 
 if __name__ == "__main__":
