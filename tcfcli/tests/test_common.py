85fecac8981a6567ba80805c6d1e3c56dbb8a1d6
jorgechen <jorgechen@tencent.com>
2019-07-11 17:48:45 +0800

    add scf init unit tests

--- tcfcli/tests/test_common.py
+++ tcfcli/tests/test_common.py
@@ -1,7 +1,7 @@
 AppID = "1259390363"
 Region = "ap-hongkong"
-SecretId = "AKIDSQDwudnunRt78y8lIbxS0JRPBWCqJVVK"
-SecretKey = "ymOpCJo2Xq01pd1xeNs0xdFl5AzgsqaG"
+SecretId = ""
+SecretKey = ""
 
 PythonDemoGithub = "https://github.com/tencentyun/scf-demo-python"
 PythonDemoProject = "demo-python"
