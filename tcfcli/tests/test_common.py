b9531748df2c78674ad5b9c2697a0deb3824213d
jorgechen <jorgechen@tencent.com>
2019-07-11 15:37:26 +0800

    change scf deploy cmd

--- tcfcli/tests/test_common.py
+++ tcfcli/tests/test_common.py
@@ -1,7 +1,7 @@
 AppID = "1259390363"
 Region = "ap-hongkong"
-SecretId = "AKIDcDfv11UcBgppfLme5KUWP6qSxGUdKTJj"
-SecretKey = "RhnQvFeztHE6znUzLZr02KTRDskSzO6f"
+SecretId = ""
+SecretKey = ""
 
 PythonDemoGithub = "https://github.com/tencentyun/scf-demo-python"
 PythonDemoProject = "demo-python"
