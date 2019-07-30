1ddcbbded6c514c4901599e0d25f37dcc777446b
dfounderliu <dfounderliu@tencent.com>
2019-07-30 20:40:38 +0800

    fix bugs

--- tcfcli/common/scf_client/scf_log_client.py
+++ tcfcli/common/scf_client/scf_log_client.py
@@ -2,6 +2,7 @@ import click
 import json
 import time
 import signal
+from tcfcli.common.operation_msg import Operation
 from tcfcli.common.user_exceptions import InvalidEnvParameters
 from tencentcloud.common.exception.tencent_cloud_sdk_exception import TencentCloudSDKException
 from tencentcloud.scf.v20180416 import scf_client, models
@@ -10,21 +11,22 @@ from . import ScfBaseClient
 
 
 class ScfLogClient(ScfBaseClient):
-
     DEFAULT_INTERVAL = 300
+
     def __init__(self, func, ns="default", region=None, err_only=None):
         super(ScfLogClient, self).__init__(region)
         self._func = func
         self._ns = ns
-        self._err_only = err_only        
+        self._err_only = err_only
 
-    #fetch in desc, print in asc
+        # fetch in desc, print in asc
+
     def fetch_log_tail_c(self, startime, endtime, count, tail):
         log_stack = []
         for logs in self.__fetch_log(startime, endtime, count, tail, order="desc"):
             for log in logs:
                 log_stack.append(log)
-        for i in range(len(log_stack)-1, -1, -1):
+        for i in range(len(log_stack) - 1, -1, -1):
             log = log_stack[i]
             click.secho(log.StartTime, fg="green")
             if log.RetCode == 0:
@@ -32,17 +34,15 @@ class ScfLogClient(ScfBaseClient):
             else:
                 click.secho(log.Log, fg="red")
 
-
     def fetch_log(self, startime, endtime, count, tail=False):
         for logs in self.__fetch_log(startime, endtime, count, tail):
             for log in logs:
-                click.secho(log.StartTime, fg="green")
+                Operation("Log startTime: " % str(log.StartTime)).process()
                 if log.RetCode == 0:
-                    click.secho(log.Log)
+                    click.secho(str(log.Log).replace("\n\n", "\n"))
                 else:
-                    click.secho(log.Log, fg="red")
-
-                
+                    click.secho(str(log.Log).replace("\n\n", "\n"), fg="red")
+                click.secho("\n\n")
 
     def __fetch_log(self, startime, endtime, count, tail, order="asc"):
         step = 1000
@@ -68,5 +68,3 @@ class ScfLogClient(ScfBaseClient):
                 time.sleep(2)
             else:
                 time.sleep(0.5)
-            
-
