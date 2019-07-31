1a8cc2462064cf04c75a78cb971a05e1c5426609
dfounderliu <dfounderliu@tencent.com>
2019-07-31 15:23:02 +0800

    add infor

--- tcfcli/cmds/delete/cli.py
+++ tcfcli/cmds/delete/cli.py
@@ -2,6 +2,7 @@ import os
 import click
 from cookiecutter.main import cookiecutter
 from cookiecutter import exceptions
+from tcfcli.common.user_config import UserConfig
 from tcfcli.common.operation_msg import Operation
 from tcfcli.common.user_exceptions import *
 import tcfcli.common.base_infor as infor
@@ -15,25 +16,22 @@ class Delete(object):
     @staticmethod
     def do_cli(region, namespace, name):
 
-        if region and region not in REGIONS:
-            raise ArgsException("The region must in %s." % (", ".join(REGIONS)))
-        else:
-            rep = ScfClient(region).get_ns(namespace)
-            if not rep:
-                raise DeleteException("Namespace {ns} not exists".format(ns=namespace))
-                # return
+        rep = ScfClient(region).get_ns(namespace)
+        if not rep:
+            raise DeleteException("Namespace {ns} not exists".format(ns=namespace))
+            # return
 
-            rep = ScfClient(region).get_function(function_name=name, namespace=namespace)
-            if not rep:
-                raise DeleteException("Function {function} not exists".format(function=name))
-                # return
+        rep = ScfClient(region).get_function(function_name=name, namespace=namespace)
+        if not rep:
+            raise DeleteException("Function {function} not exists".format(function=name))
+            # return
 
-            rep = ScfClient(region).delete_function(function_name=name, namespace=namespace)
-            if not rep:
-                raise DeleteException("Function {function} delete failed".format(function=name))
-                # return
+        rep = ScfClient(region).delete_function(function_name=name, namespace=namespace)
+        if not rep:
+            raise DeleteException("Function {function} delete failed".format(function=name))
+            # return
 
-            Operation("Function {function} delete success".format(function=name)).success()
+        Operation("Function {function} delete success".format(function=name)).success()
 
 
 def abort_if_false(ctx, param, value):
@@ -44,10 +42,9 @@ def abort_if_false(ctx, param, value):
 @click.command(short_help=help.SHORT_HELP)
 @click.option('-r', '--region', type=str, help=help.REGION)
 @click.option('-ns', '--namespace', default="default", help=help.NAMESPACE)
-@click.option('-n', '--name', required=True, help=help.NAME)
-@click.option('-f', '--force', is_flag=True, callback=abort_if_false, expose_value=False,
-              prompt='Are you sure delete this remote function?', help=help.FORCED)
-def delete(region, namespace, name):
+@click.option('-n', '--name', help=help.NAME)
+@click.option('-f', '--force', is_flag=True, help=help.FORCED)
+def delete(region, namespace, name, force):
     '''
         \b
         Delete a SCF function.
@@ -57,4 +54,26 @@ def delete(region, namespace, name):
             * Delete a SCF function
               $ scf delete --name functionname --region ap-guangzhou --namespace default
     '''
-    Delete.do_cli(region, namespace, name)
+    if name:
+
+        if not region:
+            region = UserConfig().region
+
+        if region and region not in REGIONS:
+            raise ArgsException("The region must in %s." % (", ".join(REGIONS)))
+
+        Operation("Function Information:").process()
+        Operation("  Region: %s" % (region)).process()
+        Operation("  Namespace: %s" % (namespace)).process()
+        Operation("  Function Name: %s" % (name)).process()
+
+        if force:
+            Delete.do_cli(region, namespace, name)
+        else:
+            result = click.prompt(click.style('[!] Are you sure delete this remote function? (y/n)', fg="magenta"))
+            if result in ["y", "Y"]:
+                Delete.do_cli(region, namespace, name)
+            else:
+                Operation("Delete operation has been canceled").warning()
+    else:
+        raise ArgsException("You must give a name, like: scf delete --name YourFunctionName! ")
