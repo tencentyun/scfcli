fe421d6ed10ed499bb5f5d3c8a3fc4ece17e6dbf
dfounderliu <dfounderliu@tencent.com>
2019-08-14 13:48:05 +0800

    fix bugs

--- tcfcli/libs/utils/scf_client.py
+++ tcfcli/libs/utils/scf_client.py
@@ -95,27 +95,16 @@ class ScfClient(object):
         return resp.to_json_string()
 
     def update_func_config(self, func, func_name, func_ns):
-        print(1)
         req = models.UpdateFunctionConfigurationRequest()
-        print(2)
         req.Namespace = func_ns
-        print(3)
         req.FunctionName = func_name
-        print(4)
         proper = func.get(tsmacro.Properties, {})
-        print(5)
         req.Description = proper.get(tsmacro.Desc)
-        print(6)
         req.MemorySize = proper.get(tsmacro.MemSize)
-        print(7)
         req.Timeout = proper.get(tsmacro.Timeout)
-        print(8)
         req.Environment = self._model_envs(proper.get(tsmacro.Envi, {}))
-        print(9)
         req.VpcConfig = self._model_vpc(proper.get(tsmacro.VpcConfig))
-        print(req)
         resp = self._client.UpdateFunctionConfiguration(req)
-        print(resp)
         return resp.to_json_string()
 
     def update_service_code(self, func, func_name, func_ns):
@@ -194,9 +183,9 @@ class ScfClient(object):
                 pass
             else:
                 return err
-        import time
-        time.sleep(2)
-        Operation(str(err)).warning()
+        # import time
+        # time.sleep(2)
+        # Operation(str(err)).warning()
         Operation("{ns} {name} already exists, update it now".format(ns=func_ns, name=func_name)).process()
         try:
             # if 'Type' in func['Properties'] and func['Properties']['Type'] == 'HTTP' and \
@@ -205,10 +194,10 @@ class ScfClient(object):
             # self.update_service_code(func, func_name, func_ns)
             # else:
             res = self.update_func_config(func, func_name, func_ns)
-            Operation(str(res))
+            # Operation(str(res))
             Operation("Update funcion config success.").process()
             res = self.update_func_code(func, func_name, func_ns)
-            Operation(str(res))
+            # Operation(str(res))
             Operation("Update funcion code success.").process()
         except TencentCloudSDKException as err:
             return err
