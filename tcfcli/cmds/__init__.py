799918d75fde08800fd7db41dde61c48d2eefea3
syz <eagle-china@hotmail.com>
2019-11-22 10:19:57 +0800

    update version number

--- tcfcli/cmds/__init__.py
+++ tcfcli/cmds/__init__.py
@@ -3,7 +3,7 @@
 from tcfcli.common import statistics, daily_task
 from tcfcli.common.operation_msg import Operation
 from tcfcli.common.daily_task import get_version
-from tcfcli.cmds.cli import __version__
+from tcfcli.cmds.cli import __version__, __internal_version__
 
 import sys
 
@@ -21,7 +21,7 @@ if "-v" in sys.argv or "--version" in sys.argv:
     except:
         pass
     if "-test" in sys.argv:
-        Operation("Test Version: %s" % ("0.2.2.3"), tofile=False).success()
+        Operation("Test Version: %s" % (__internal_version__), tofile=False).success()
     sys.exit(0)
 else:
     daily_task.daily_task()
--- tcfcli/cmds/cli/__init__.py
+++ tcfcli/cmds/cli/__init__.py
@@ -4,4 +4,5 @@
 TCF CLI VERSION
 """
 
-__version__ = '0.2.3'
+__version__ = '0.2.4'
+__internal_version__ = '0.2.4.1'
